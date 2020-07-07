9933ae4314743417775a231f781a4589878cd535
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-07 11:15:50 +0900

    inte

--- db/migrate/20200524110429_create_posts.rb
+++ db/migrate/20200524110429_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title, null: false, default: ""
       t.text   :review, null: false
-      t.references :user, foreign_key: true, type: :bingint
+      t.references :user, foreign_key: true, type: :bigint
       t.references :anime, foreign_key: true
       t.float      :rate, null: false
       t.timestamps
