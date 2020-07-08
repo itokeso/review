eb6a38f0e159baf8e7c6c87dcf4bf527a8b3c54c
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-08 15:59:25 +0900

    fix

--- db/migrate/20200526111916_create_animes.rb
+++ db/migrate/20200526111916_create_animes.rb
@@ -2,7 +2,7 @@ class CreateAnimes < ActiveRecord::Migration[5.2]
   def change
     create_table :animes do |t|
       t.string :title, null: false
-      t.string :genre, null: false
+      t.string :genre
       t.text  :image, null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_06_28_085247) do
 
   create_table "animes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
-    t.string "genre", null: false
+    t.string "genre"
     t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
