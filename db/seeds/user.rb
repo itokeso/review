405903dbd6dcbed77bd68aad0c3bf95d94acd39f
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-09 15:33:38 +0900

    test

---
+++ db/seeds/user.rb
@@ -0,0 +1,4 @@
+User.create(name: "佐藤巧", email:"headwaybasket@gmail.com", password: "11111111")
+User.create(name: "田中太郎", email:"headwaybasket@yahoo.co.jp", password: "11111111")
+User.create(name: "鈴木圭", email: "suzuki@gmail.com", password: "11111111")
+User.create(name: "堀江愛", email: "horie@gmail.com", password: "11111111")
