a8f95441c92490c3de03035cc01210c27fb86415
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-17 22:03:56 +0900

    fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,7 @@ describe User do
     it "メールアドレスがないと登録できないこと" do
       user = User.new(name: "伊藤", email: nil, password:"00000000", password_confirmation: "00000000")
       user.valid?
-      # expect(user.errors[:email]).to include("を入力してください")
+      expect(user.errors[:email]).to include("を入力してください")
     end
   end
 end
