bc3469a2e7a385dfc7e5fc653283fff8f0ad2188
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-19 18:06:47 +0900

    fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
-# require 'rails_helper'
-# describe User do
-#   describe '#create' do
-#     it "名前がないと登録できないこと" do
-#       user = User.new(name: nil, email: "itou@gmail.com", password:"00000000", password_confirmation: "00000000")
-#       user.valid?
-#       expect(user.errors[:name]).to include("を入力してください")
-#     end
-#     it "メールアドレスがないと登録できないこと" do
-#       user = User.new(name: "伊藤", email: nil, password:"00000000", password_confirmation: "00000000")
-#       user.valid?
-#       expect(user.errors[:email]).to include("を入力してください")
-#     end
-#   end
-# end
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "名前がないと登録できないこと" do
+      user = User.new(name: nil, email: "itou@gmail.com", password:"00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+    it "メールアドレスがないと登録できないこと" do
+      user = User.new(name: "伊藤", email: nil, password:"00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+  end
+end
