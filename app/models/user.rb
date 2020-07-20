c9da95f9b5cd13368e5a7a22f5155e152d998eaa
itokeso <61177741+itokeso@users.noreply.github.com>
2020-07-20 23:14:37 +0900

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 12 }
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "名前とメールアドレスとパスワードとパスワード確認があれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
     it "名前がないと登録できないこと" do
       user = build(:user, name: nil)
       user.valid?
@@ -10,6 +14,26 @@ describe User do
       user = build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
+    end
+    it "パスワードがないと登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+    it "パスワードが8文字未満だと登録できないこと" do
+      user = build(:user, password: "asdedc")
+      user.valid?
+      expect(user.errors[:password]).to include("は8文字以上で入力してください")
+    end
+    it "パスワードがあってもパスワード確認がなければ登録できないこと" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+    it "名前が12文字以上だと登録できないこと" do
+      user = build(:user, name: "あああああああああああああ") 
+      user.valid?
+      expect(user.errors[:name]).to include("は12文字以内で入力してください")
     end
   end
 end
